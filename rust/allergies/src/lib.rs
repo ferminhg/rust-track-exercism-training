d03742ae3fbb0c3522f39a14d3be491bd74bc77f
Fermin <josefermin.hernandez@adevinta.com>
2022-10-30 09:03:27 +0100

    add book example and middle exercise

--- rust/allergies/src/lib.rs
+++ rust/allergies/src/lib.rs
@@ -1,33 +1,57 @@
-pub struct Allergies;
+pub struct Allergies {
+    list_allergies: Vec<Allergen>
+}
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Allergen {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats,
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128,
+}
+
+fn allergeis_by_score(score: u32) -> Vec<Allergen> {
+    let mut allergies = Vec::<Allergen>::new();
+
+    if score <= 0 {
+        return allergies;
+    }
+    if score == 2 { return Vec::from([Allergen::Peanuts]); }
+    if score == 4 { return Vec::from([Allergen::Shellfish]); }
+    if score == 8 { return Vec::from([Allergen::Strawberries]); }
+
+    if score >= 128 { allergies.push(Allergen::Cats) }
+    if score >= 64 { allergies.push(Allergen::Pollen) }
+    if score >= 32 { allergies.push(Allergen::Chocolate) }
+    if score >= 16 { allergies.push(Allergen::Tomatoes) }
+    if score >= 8 { allergies.push(Allergen::Strawberries) }
+    if score >= 4 { allergies.push(Allergen::Shellfish) }
+    if score >= 2 { allergies.push(Allergen::Peanuts) }
+    if score >= 1 { allergies.push(Allergen::Eggs) }
+
+    return allergies;
 }
 
 impl Allergies {
     pub fn new(score: u32) -> Self {
-        unimplemented!(
-            "Given the '{}' score, construct a new Allergies struct.",
-            score
-        );
+        let allergies = allergeis_by_score(score);
+        Self{list_allergies: allergies.to_vec()}
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        unimplemented!(
-            "Determine if the patient is allergic to the '{:?}' allergen.",
-            allergen
-        );
+        for i in 0 .. self.list_allergies.len() {
+            if self.list_allergies[i] == *allergen {
+                return true;
+            }
+        }
+        false
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
-        unimplemented!("Return the list of allergens contained within the score with which the Allergies struct was made.");
+        Vec::from(self.list_allergies.to_vec())
     }
 }
