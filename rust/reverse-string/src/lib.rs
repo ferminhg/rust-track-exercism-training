b4228f6cfcf57f757c6451d36b5a64b6ac08db58
Fermin <josefermin.hernandez@adevinta.com>
2022-11-11 21:59:27 +0100

    improve solution

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,8 +1,4 @@
 #[warn(unused_variables)]
 pub fn reverse(input: &str) -> String {
-    let mut reverse = String::new();
-    for char in input.chars() {
-        reverse.insert(0, char);
-    }
-    reverse.to_string()
+    input.chars().rev().collect::<String>()
 }
