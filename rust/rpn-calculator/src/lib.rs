b9cfdc77361c60617ff00ee9517911a6ebeb61d3
Fermin <josefermin.hernandez@adevinta.com>
2022-11-11 21:18:28 +0100

    refactor

--- rust/rpn-calculator/src/lib.rs
+++ rust/rpn-calculator/src/lib.rs
@@ -9,27 +9,38 @@ pub enum CalculatorInput {
 
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
     let mut stack: Vec<i32> = Vec::new();
-    for input in inputs {
-        match input {
-            CalculatorInput::Value(value)=> stack.push(*value),
-            _ => {
-                if stack.len() < 2 { return None; }
-                let b = stack.pop().unwrap();
-                let a = stack.pop().unwrap();
-
-                let value = match input {
-                    CalculatorInput::Add => a + b,
-                    CalculatorInput::Subtract => a - b,
-                    CalculatorInput::Multiply => a * b,
-                    CalculatorInput::Divide => a / b,
-                    _ => panic!("should not be reached"),
-                };
-                stack.push(value);
-            }
-        }
-    }
+    inputs_calculator(inputs, &mut stack);
     if stack.len() != 1 {
         return None;
     }
     return stack.pop()
+}
+
+fn inputs_calculator(inputs: &[CalculatorInput], stack: &mut Vec<i32>) {
+    for input in inputs {
+        match input {
+            CalculatorInput::Value(value) => stack.push(*value),
+            _ => {
+                if stack.len() < 2 {
+                    stack.truncate(0);
+                    break;
+                }
+                let b = stack.pop().unwrap();
+                let a = stack.pop().unwrap();
+
+                let value = value_calculator(input, b, a);
+                stack.push(value);
+            }
+        }
+    }
+}
+
+fn value_calculator(input: &CalculatorInput, b: i32, a: i32) -> i32 {
+    match input {
+        CalculatorInput::Add => a + b,
+        CalculatorInput::Subtract => a - b,
+        CalculatorInput::Multiply => a * b,
+        CalculatorInput::Divide => a / b,
+        _ => panic!("should not be reached"),
+    }
 }
