e1d8a25a323005361e93a16f8f85e4d6b94fe5c7
Fermin <josefermin.hernandez@adevinta.com>
2022-11-23 23:03:03 +0100

    solution to proverb exercise

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,20 @@
+const LAST_SENTENCE: &str = "And all for the want of a";
+
+pub fn build_longer_proverb(list: &[&str]) -> String {
+    let mut proverb:Vec<String> = vec![];
+    let mut i = 0;
+    while i <= list.len()-2 {
+        proverb.push(format!("For want of a {} the {} was lost.", list[i], list[i+1]));
+        i += 1;
+    }
+    proverb.push(format!("{LAST_SENTENCE} {}.", list[0]));
+    proverb.join("\n")
+}
+
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    match list.len() {
+        0 => String::new(),
+        1 => format!("{LAST_SENTENCE} {}.", list[0]),
+        _ => build_longer_proverb(list),
+    }
 }
