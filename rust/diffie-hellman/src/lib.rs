6ab3ecfa90f17fdce9b94342a14df6308dd2174f
Fermin <josefermin.hernandez@adevinta.com>
2022-12-09 21:41:10 +0100

    diffie hellman exercise

---
+++ rust/diffie-hellman/src/lib.rs
@@ -0,0 +1,28 @@
+use rand::Rng;
+
+pub fn private_key(p: u64) -> u64 {
+    rand::thread_rng().gen_range(2..p)
+}
+
+pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
+    mod_pow(g, a, p)
+}
+
+pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
+    mod_pow(b_pub, a, p)
+}
+
+fn mod_pow(base: u64, exp: u64, modulus: u64) -> u64 {
+    if modulus == 1 { return 0; }
+    let mut result: u64 = 1;
+    let mut base = base % modulus;
+    let mut exp = exp;
+    while exp > 0 {
+        if exp % 2 == 1 {
+            result = (result * base) % modulus;
+        }
+        exp /= 2;
+        base = (base * base) % modulus;
+    }
+    result
+}
